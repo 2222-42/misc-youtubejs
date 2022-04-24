ba951b778f019ecc4dfaa445a8cf05bb216b847c
2222-42 <stephen0617@me.com>
2022-04-24 22:27:52 +0900

    feat(*): add livechat.js
    Liveのチャット情報を見る。

---
+++ livechat.js
@@ -0,0 +1,31 @@
+const Innertube = require('youtubei.js');
+
+async function start() {
+    const videoId = process.env.VIDEO_ID;
+    if (videoId === '') {
+        console.log('Video ID is required');
+        return;
+    }
+
+  const youtube = await new Innertube();
+
+  const video = await youtube.getDetails(videoId);
+  
+  const livechat = video.getLivechat();
+
+  // Updated stats about the livestream
+  livechat.on('update-metadata', (data) => {
+    console.info('Info:', data);
+  });
+   
+  // Fired whenever there is a new message or other chat events
+  livechat.on('chat-update', (message) => {
+    console.info(`- ${message.author.name}: ${message.text}\n\n`);
+    
+    if(message.text == '!info') {
+      livechat.sendMessage('Hello! This message was sent from YouTube.js');
+    }
+  });
+}
+
+start();
