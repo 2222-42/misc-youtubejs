7dece89a38ccb925b01478c4ec3c64df17d1e011
2222-42 <stephen0617@me.com>
2022-04-24 22:15:04 +0900

    chore(*): set env QUALITY
    ざっくりしたvalidationも導入

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 const Innertube = require('youtubei.js');
 
+function isInt(str) {
+    let x = parseFloat(str);
+    return !isNaN(str) && (x | 0) === x;
+}
+
 async function start() {
+    const quality = process.env.QUALITY || '360'
+    if (!isInt(quality)) {
+      console.log('Quality must be an integer');
+      return;
+    }
+    
   const youtube = await new Innertube();
 
   const video = await youtube.getDetails(process.env.VIDEO_ID);
   
   const stream = youtube.download(video.id, {
     format: 'mp4', // Optional, defaults to mp4 and I recommend to leave it as it is unless you know what you're doing
-    quality: '360p', // if a video doesn't have a specific quality it'll fall back to 360p, also ignored when type is set to audio
+    quality: quality + 'p', // if a video doesn't have a specific quality it'll fall back to 360p, also ignored when type is set to audio
     type: 'videoandaudio' // can be “video”, “audio” and “videoandaudio”
   });
   
